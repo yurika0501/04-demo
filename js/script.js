87d3c0ab8ce2d66c72cc45987e8307b42dbb43e3
yurika0501 <titizuzu0501@gmail.com>
2023-05-06 12:50:54 +0000

    [Add]ボタンのテキスト変更

--- js/script.js
+++ js/script.js
@@ -2,4 +2,14 @@ const btn = document.querySelector('#btn');
 btn.addEventListener('click', () => {
     //クリックされたときの処理
     document.body.classList.toggle('dark-theme');
+    // if ボタンのテキストが「ダークモードにする」になっているなら
+    if (btn.textContent === 'ダークモードにする'){
+        // クリックされたときに「ライトモード」に変更
+        btn.textContent = 'ライトモードにする';
+        
+    // そうでないなら(「ライトモードにする」と表示されているなら
+    } else {
+        // クリックされたときに「ダークモードにする」に戻す
+        btn.textContent = 'ダークモードにする';
+    }
 });
