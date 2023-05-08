d9f97e05c8a02420005fc5080f1ee3659192dd76
yurika0501 <titizuzu0501@gmail.com>
2023-05-08 01:21:40 +0000

    [Add]規定文字数超えた場合、赤文字にする

--- js/script.js
+++ js/script.js
@@ -20,4 +20,14 @@ btn.addEventListener('click', () => {
 text.addEventListener('keyup', () => {
     // キー入力されたときの処理
     count.textContent = text.value.length;
-})
+    
+    // 100文字を超えるなら
+    if (text.value.length > 100) {
+        // alertクラスを加える
+        count.classList.add('alert');
+        // そうでないなら（100文字以下なら）
+    } else {
+        // alertクラスを外す
+        count.classList.remove('alert')
+    }
+});
